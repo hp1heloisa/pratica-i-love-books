a44dea360ae69d5b9ddf49aafe3be3cb79f9e420
hp1heloisa <heloisapimentel2202@gmail.com>
2023-09-22 17:38:34 -0300

    entrega

---
+++ src/repositories/books-repository.ts
@@ -0,0 +1,35 @@
+import { Book, CreateBook } from "../protocols/book";
+import { CreateReview } from "../protocols/review";
+
+import connection from "../database";
+import prisma from "../database";
+
+export async function getBooks() {
+  const result = await prisma.books.findMany();
+  return result;
+}
+
+export async function getBook(id: number) {
+  const result = await prisma.books.findUnique({
+    where:{
+      id
+    }
+  });
+  return result;
+}
+
+export async function createBook(book: CreateBook) { 
+  const result = await prisma.books.create({
+    data: book
+  });
+  return result;
+}
+
+export async function reviewBook(bookReview: CreateReview) {
+  const { bookId, grade, review } = bookReview;
+  const result = await prisma.books.update(({
+    data: {grade, review},
+    where: {id: bookId}
+  }))
+  return result;
+}
